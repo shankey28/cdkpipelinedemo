c9f17f0af84a95cc4ec6d8704c4c9d8be9126402
shankey28 <shashiishankar@gmail.com>
2021-05-04 11:45:35 -0400

    Fixed Lambda path

--- lib/cdkpipelines-demo-stack.ts
+++ lib/cdkpipelines-demo-stack.ts
@@ -11,7 +11,7 @@ export class CdkpipelinesDemoStack extends cdk.Stack {
     super(scope, id, props);
 
     const handler = new lambda.Function(this,'HelloWorld',{
-      code: lambda.Code.fromAsset(path.resolve(__dirname,'lambda')),
+      code: lambda.Code.fromAsset(path.resolve(__dirname,'../lambda')),
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'handler.hello'
 
